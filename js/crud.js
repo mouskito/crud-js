0ceeaf4a0605fbc653843e6601e63ad9785ac28e
Moussa Camara <65198569+zvrsd@users.noreply.github.com>
2021-02-09 10:53:52 +0100

    verification email si mail existe

--- js/crud.js
+++ js/crud.js
@@ -12,7 +12,12 @@ function champsVides() {
 	if(prenom === '' || nom === '' || email === ''){
 		console.log('Oups')
 	}else{
-		creationLigne(prenom,nom,email);
+		if(verifEmail(email) === false){
+			console.log('email déja présent');
+			$("#erreur").html('<p style="color:red"> Le mail existe déja</p>')
+		}else{
+			creationLigne(prenom,nom,email);
+		}
 	}
 }
 
@@ -28,4 +33,19 @@ function creationLigne(a,b,c) {
 	var ligne_prenom =nouvelleLigne.insertCell(1).innerHTML = a;
 	var ligne_nom =nouvelleLigne.insertCell(2).innerHTML = b;
 	var ligne_email =nouvelleLigne.insertCell(3).innerHTML = c;
+}
+
+// Verification email
+function verifEmail(email) {
+	var table = document.querySelector('.table');
+	var nombre_ligne = table.rows.length -1;
+
+	for (var index = 1; index <= nombre_ligne; index++) {
+		var email_tableau = table.getElementsByTagName('tr')[index].cells[3].innerHTML;
+		console.log(email_tableau)
+
+		if(email === email_tableau){
+			return false;
+		}
+	}
 }
